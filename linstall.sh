9855275341b05438afb725c88dcea29715e73621
xvass <xenofonv@gmail.com>
2017-08-29 09:14:01 +0300

    linstall script added

---
+++ linstall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# generate a run script which performing the build process of UserManager application
+
+DIRECTORY=`pwd | cut -d/ -f4`
+
+if [ "$DIRECTORY" != "UserManager" ]; then 
+	echo " this isnt the UserManager Directory " 
+	exit 0
+fi
+while (( inotifywait -e modify *.cpp || inotifywait -e modify *.h )) ; do
+make clean 
+rm -rf Makefile > /dev/null 2>&1
+qmake -project
+qmake -o Makefile UserManager.pro 
+echo "QMAKE_CXXFLAGS += -std=c++11" >> UserManager.pro
+echo "CONFIG += qt debug" >> UserManager.pro
+echo "QMAKE_LIBS += -lcrypt" >> UserManager.pro
+echo "# installation" >> UserManager.pro
+echo "installfiles.files += usermanager" >> UserManager.pro
+echo "installfiles.path += /usr/bin/" >> UserManager.pro  
+echo "INSTALLS += installfiles" >> UserManager.pro
+make
+make install
+#UserManager 
+done
+
+
+
